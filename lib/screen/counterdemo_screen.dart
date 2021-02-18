7e797b2bbaeb258357b1bffb93f526aef23f72b3
Mohtashim <rohithaider@gmail.com>
2021-02-18 11:31:58 -0600

    setState

--- lib/screen/counterdemo_screen.dart
+++ lib/screen/counterdemo_screen.dart
@@ -9,13 +9,43 @@ class CounterDemoScreen extends StatefulWidget {
 }
 
 class _CounterDemoState extends State<CounterDemoScreen> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter Demo'),
       ),
-      body: Text('Counter Demo'),
+      body: Column(
+        children: [
+          Text(
+            'Counter = $count',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                ++count;
+              });
+            },
+            child: Text(
+              'Up',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                --count;
+              });
+            },
+            child: Text(
+              'Down',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
