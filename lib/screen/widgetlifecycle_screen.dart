7c8433948b890f134c64096dba9cd2bfdc768289
Mohtashim <rohithaider@gmail.com>
2021-02-18 12:28:54 -0600

    life cycle

--- lib/screen/widgetlifecycle_screen.dart
+++ lib/screen/widgetlifecycle_screen.dart
@@ -4,18 +4,45 @@ class WidgetLifeCycleScreen extends StatefulWidget {
   static const routeName = '/widgetLifeCycleScreen';
   @override
   State<StatefulWidget> createState() {
+    print('================= createState()');
     return _WidgetLifeCycleState();
   }
 }
 
 class _WidgetLifeCycleState extends State<WidgetLifeCycleScreen> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('======initState(): reserve resources ');
+  }
+
+  @override
+  void dispose() {
+    print('======dispose(): release reserved resources');
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('======Build()');
     return Scaffold(
       appBar: AppBar(
         title: Text('Widget Life Cycle Demo'),
       ),
-      body: Text('Life Cycle'),
+      body: Column(children: [
+        Text('Count: $count'),
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              ++count;
+            });
+          },
+          child: Text('Up'),
+        )
+      ]),
     );
   }
 }
