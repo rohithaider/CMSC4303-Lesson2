75e81eb9b37060ea3aa32a7c6e5b2b380fa74faa
Mohtashim <rohithaider@gmail.com>
2021-02-24 19:38:29 -0600

    add drawer to user home

--- lib/screen/userhome_screen.dart
+++ lib/screen/userhome_screen.dart
@@ -11,14 +11,53 @@ class UserHomeScreen extends StatefulWidget {
 
 class _UserHomeState extends State<UserHomeScreen> {
   UserRecord userRecord;
+  _Controller con;
+
+  @override
+  void initState() {
+    super.initState();
+    con = _Controller(this);
+  }
+
   @override
   Widget build(BuildContext context) {
     userRecord = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Home Screen'),
+        title: Text('Home for ${userRecord.name}'),
       ),
-      body: Text(userRecord.toString()),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text(userRecord.email),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Sign Out'),
+              onTap: null,
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Text(
+            'Hi, ${userRecord.name}',
+            style: Theme.of(context).textTheme.headline5,
+          )
+        ],
+      ),
     );
   }
+}
+
+class _Controller {
+  _UserHomeState state;
+  _Controller(this.state);
 }
