22bfd7a3c56a4b7c54df478ff2d006e2f03e1e3a
Mohtashim <rohithaider@gmail.com>
2021-02-25 13:31:16 -0600

    modal

--- lib/screen/profile_scree.dart
+++ lib/screen/profile_scree.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lesson2/model/user_record.dart';
 
 class ProfileScreen extends StatefulWidget {
   static const routeName = '/profileScreen';
@@ -10,6 +11,8 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileState extends State<ProfileScreen> {
   _Controller con;
+  UserRecord userRecord;
+  bool editMode = false;
 
   @override
   void initState() {
@@ -23,9 +26,15 @@ class _ProfileState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    userRecord = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
+        actions: [
+          editMode
+              ? IconButton(icon: Icon(Icons.check), onPressed: con.update)
+              : IconButton(icon: Icon(Icons.edit), onPressed: con.edit)
+        ],
       ),
       body: Text('profile'),
     );
@@ -35,4 +44,12 @@ class _ProfileState extends State<ProfileScreen> {
 class _Controller {
   _ProfileState state;
   _Controller(this.state);
+
+  void edit() {
+    state.render(() => state.editMode = true);
+  }
+
+  void update() {
+    state.render(() => state.editMode = false);
+  }
 }
